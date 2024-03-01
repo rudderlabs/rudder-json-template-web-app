f2b406ca587c8aa6f0a420692622c5428e7783e3
Dilip Kola <kdilipkola@gmail.com>
2024-03-01 21:34:33 +0530

    chore: downgrade workflow engine

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import dynamicImport from 'vite-plugin-dynamic-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     nodePolyfills({
       include: ['path'],
     }),
+    dynamicImport(),
   ],
 });
